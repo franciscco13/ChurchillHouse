e64e17cb268800bc982d5650920e3d7efc1f26e5
Francisco Trejo <frtrma@outlook.com>
2016-03-06 16:53:30 -0600

    v1.9
    Agregado menú telefónico flotante
    Agregada galería en harrogate.html
    Agregado videos finales a index
    Agregado menus desplegables en footer
    Agregado botón que envía a app movil (sin funcionalidad aún)
    Agregado favicon
    Corregido visores de imagenes
    Corregido links del menu lateral en versión movil
    Corregido detalles menores de summer.html y harrogate.html
    Corregido tamaño de los logos de patrocinadores
    Optimización de imagenes del sitio
    Quitado menús desplegables de navbar

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Churchill House Page - Front End
 
 v1.0
 -------------------
-index.html acabado.
+Agregado index.html.
 
 v.1.3
 -------------------
@@ -17,7 +17,6 @@ Agregado summer.html (version semi-final)<br>
 Agregado links entre páginas<br>
 Corregido detalles de responsividad
 
-<<<<<<< HEAD
 v.1.5
 -------------------
 Agregado harrogate.html (version semi-final)<br>
@@ -25,8 +24,18 @@ Agregado contact.html (version semi-final)<br>
 Agregado navbar con menus desplegables<br> 
 Corregido detalles de responsividad
 
-=======
-v.1.38
+v1.9 
 -------------------
-Agregadas fotos finales del proyecto
->>>>>>> origin/master
+Agregado menú telefónico flotante
+Agregada galería en harrogate.html
+Agregado videos finales a index 
+Agregado menus desplegables en footer
+Agregado botón que envía a app movil (sin funcionalidad aún)
+Agregado favicon
+Corregido visores de imagenes
+Corregido links del menu lateral en versión movil
+Corregido detalles menores de summer.html y harrogate.html
+Corregido tamaño de los logos de patrocinadores
+Optimización de imagenes del sitio
+Quitado menús desplegables de navbar
+
--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@ $(function(){
 
 	$(".button-collapse").sideNav(); 
 
-	/** Redireccionar al index si se da clic en logo  **/
-	$(".nav-wrapper .logo").on("click", function(){ 
-		window.location.href = "./index.html";
-	});
+
+ 
 
 	$(".navbtns a").hover(function(){
 		var menu = $(this).attr("data-activates");
@@ -32,4 +30,156 @@ $(function(){
 		menu.hide();
 	});
 
+	//Ocultar boton de llamadas
+	// //Firefox
+	//  
+	var phone = $(".phone_button");
+	var auxup = 0;
+	var auxdown = 0;
+
+	$('body').bind('DOMMouseScroll', function(e){
+	    if(e.originalEvent.detail > 0) {
+	    	
+	    	auxup = (auxup < 5) ? auxup+1 : 0; 
+	        //scroll down
+	        if(auxup == 5)
+	        	phone.addClass('disappear');
+	    }else {
+	    	auxdown = (auxdown < 5) ? auxdown+1 : 0; 
+	        //scroll up
+	        if(auxdown == 5)
+	        	phone.removeClass('disappear');
+	    }
+	    //prevent page fom scrolling
+	    return false;
+	});
+
+	 //IE, Opera, Safari
+	$('body').bind('mousewheel', function(e){
+	    if(e.originalEvent.wheelDelta < 0) {
+	       //scroll down
+	        phone.addClass('disappear');
+	    }else {
+	        //scroll up
+	       phone.removeClass('disappear');
+	    }
+ 
+	});
+
+	var dropbtn = $(".droplist"); 
+
+	dropbtn.click(function(){
+		$(this).parent().find("li").toggle(400);
+		$(this).parent().find(".ico-up").toggle(400);
+		$(this).parent().find(".ico-down").toggle(400);
+	});
+
+
+	//Abrir visor
+	var visor = $(".image-viewer");
+	var ws = {};
+	var imgs = {};
+ 	var origs = {};
+ 	var parent;
+	var main_img = visor.find(".main-image");
+
+
+	$(".imgpreview").on("click", function(){
+
+		//Toggle visor
+		visor.fadeIn(300);
+		//Almacena la imagen en una variable
+		var img = $(this).find("img");
+		//Almacena la url de la imagen en una variable
+		var src = img.attr("src"); 
+		//consigue la altura y ancho original de la imagen
+		origs["w"] = imgs["w"] = img[0].width;
+		origs["h"] = imgs["h"] = img[0].height; 
+		//Establece un tamaño adecuado de la imagen dentro del visor
+		normalize();		 
+		//Coloca la imagen como fondo del visor.
+		main_img.css("background-image", "url("+src+")");
+		
+		//Consigue cuando miniaturas se mostrarán
+		parent = $(this).parent();
+		var thumbs = parent.find(".imgpreview");
+		thumbs.each(function(index, value){
+			var img_thumb = $(this).find("img").attr('src');
+			visor.find(".previews").append('<div class="thumb z-depth-1" src = "'+img_thumb+'"></div>');
+		});
+
+		$(".thumb").each(function(){
+			var thumb_src = $(this).attr('src');
+			if(thumb_src == src )
+				$(this).addClass("active");
+			$(this).css("background-image", "url("+thumb_src+")");
+		});
+	});
+
+
+	//Cambiar de imagen al dar clic a thumbnail
+	$("body").on("click", ".thumb", function(){
+		var src = $(this).attr("src");		
+		$(".thumb").removeClass("active");
+		$(this).addClass("active");
+		main_img.css("background-image", "url("+src+")");
+
+		var length = parent.find(".imgpreview").each(function(index, value){
+			var img_thumb = $(this).find("img"); 
+			if(src == img_thumb.attr('src')){ 
+				//consigue la altura y ancho original de la imagen 
+				origs["w"] = imgs["w"] = img_thumb[0].width;
+				origs["h"] = imgs["h"] = img_thumb[0].height; 		
+				setTimeout(function(){
+					normalize()
+				}, 200);				 
+				
+			}
+		});
+	});
+
+
+	function close_viewer(){
+		main_img.width(0);
+		main_img.height(0);
+		visor.fadeOut(200);
+		visor.find(".previews .thumb").remove();
+	}
+
+	visor.find(".close").click(function(){
+		close_viewer();
+	});
+
+	$(window).resize(function(){
+		normalize();
+	});
+
+	var pixels = 0;
+	$(window).scroll(function(){
+		pixels ++;
+		if(visor.css("display") == "block" &&  pixels > 30){
+			close_viewer();
+			pixels = 0;
+		}  
+	});
+
+	//Evita que la imagen ocupe mas espacio del que hay en pantalla
+	function normalize(){ 
+		ws["w"] = $(window).width();
+		ws["h"] = $(window).height() - 120;
+		imgs["w"] = origs.w;
+		imgs["h"] = origs.h;
+
+		while(imgs.w > ws.w || imgs.h > ws.h){
+			imgs.w *= 0.9;
+			imgs.h *= 0.9;	}
+
+		main_img.width(imgs.w);
+		main_img.height(imgs.h);
+	}
+
+
+
+
+ 
 });
